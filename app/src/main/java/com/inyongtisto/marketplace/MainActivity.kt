550a531a9b49354bb31c4c40a0763d8dd9d80635
Inyong Tisto <imtisto@gmail.com>
2021-09-01 00:24:01 +0700

    - Ini tutorial ke 4 di sesi coning tokopedia

--- app/src/main/java/com/inyongtisto/marketplace/MainActivity.kt
+++ app/src/main/java/com/inyongtisto/marketplace/MainActivity.kt
@@ -2,10 +2,14 @@ package com.inyongtisto.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("RESPOM", "PESAN SINGKAT")
+
     }
 }
